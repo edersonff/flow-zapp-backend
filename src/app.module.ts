b01e949e3aca9679cb9239c4e161af676e4e11fe
edersonff <=>
2024-05-12 03:10:50 -0300

    feat: db

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,29 +9,13 @@ import { NumbersModule } from './numbers/numbers.module';
 import { LogsModule } from './logs/logs.module';
 import { FlowModule } from './flow/flow.module';
 import { DiagramModule } from './diagram/diagram.module';
-import { User } from './users/entities/user.entity';
-import { Diagram } from './diagram/entities/diagram.entity';
-import { Flow } from './flow/entities/flow.entity';
-import { Log } from './logs/entities/log.entity';
-import { NumberEntity } from './numbers/entities/number.entity';
 import { JobsModule } from './jobs/jobs.module';
-import { Job } from './jobs/entities/job.entity';
+import { dataSourceOptions } from './db/data-source';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: (process.env.DB_TYPE as 'mysql') || 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: +process.env.DB_PORT || 3306,
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASS || 'root',
-      entities: [User, Diagram, Flow, Job, Log, NumberEntity],
-      database: process.env.DB_NAME || 'flow-zapp',
-      synchronize: false,
-      logging: true,
-      ssl: process.env.DB_SSL === 'true',
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     AuthModule,
     UsersModule,
     NumbersModule,
